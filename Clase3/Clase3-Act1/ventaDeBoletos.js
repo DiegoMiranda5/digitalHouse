46666ce3d2e2325b0f7eac166d787686d8d2c89e
DiegoMiranda5 <diegomiranda610@gmail.com>
2023-09-02 22:03:53 -0300

    Se agrego una salida por consola mas adonde muestra las variables, el dato que almacena y el tipo de varible en un formato de tabla

--- Clase3/Clase3-Act1/ventaDeBoletos.js
+++ Clase3/Clase3-Act1/ventaDeBoletos.js
@@ -13,7 +13,7 @@ let nombre;
 let apellido;
 let edad;
 let tel;
-let socio;
+let isSocio;
 let fechaDePartido;
 let horaDelPartido;
 let numSilla;
@@ -23,16 +23,32 @@ nombre = "Paquita";
 apellido = "Gomez";
 edad = 23;
 tel = 385_538_9883;
-socio = false;
+isSocio = false;
 fechaDePartido = "07/10/2023";
 horaDelPartido = "10:30";
 numSilla = 56;
 precioDelBoleto = 7500;
 
-console.log(`Nombre y apellido: ${nombre}, ${apellido}
-edad: ${edad}
-telefono: ${tel}
-es Socio: ${socio ? "si" : "no"}
-Numero de silla: ${numSilla}
-Fecha del partido: ${fechaDePartido} a las ${horaDelPartido}
-precio del boleto: ${precioDelBoleto}`);
+console.log(
+    `Nombre y apellido: ${nombre}, ${apellido}\n`+
+    `Edad: ${edad}\n` +
+    `Telefono: ${tel}\n` +
+    `Es Socio: ${isSocio ? "si" : "no"}\n` +
+    `Numero de silla: ${numSilla}\n` +
+    `Fecha del partido: ${fechaDePartido} a las ${horaDelPartido}\n` +
+    `Precio del boleto: ${precioDelBoleto}\n`);
+
+console.log(
+    `-`.repeat(60) + "\n" + 
+    `Variable`.padEnd(20)          + " | " + `Datos`.padEnd(20)              + " | " + `Tipo\n` +
+    `-`.repeat(60) + "\n" + 
+    `Nombre`.padEnd(20)            + " | " + `${nombre}`.padEnd(20)          + " | " + `${typeof(nombre)}\n`+
+    `Apellido`.padEnd(20)          + " | " + `${apellido}`.padEnd(20)        + " | " + `${typeof(apellido)}\n`+
+    `Edad`.padEnd(20)              + " | " + `${edad}`.padEnd(20)            + " | " + `${typeof(edad)}\n` +
+    `Telefono`.padEnd(20)          + " | " + `${tel}`.padEnd(20)             + " | " + `${typeof(tel)}\n` +
+    `Es socio`.padEnd(20)          + " | " + `${isSocio}`.padEnd(20)         + " | " + `${typeof(isSocio)}\n` +
+    `Numero de silla`.padEnd(20)   + " | " + `${numSilla}`.padEnd(20)        + " | " + `${typeof(numSilla)}\n` +
+    `Fecha del patido`.padEnd(20)  + " | " + `${fechaDePartido}`.padEnd(20)  + " | " + `${typeof(fechaDePartido)}\n` +
+    `Hora del partido`.padEnd(20)  + " | " + `${horaDelPartido}`.padEnd(20)  + " | " + `${typeof(horaDelPartido)}\n` +
+    `Precio del boleto`.padEnd(20) + " | " + `${precioDelBoleto}`.padEnd(20) + " | " + `${typeof(precioDelBoleto)}`
+);
