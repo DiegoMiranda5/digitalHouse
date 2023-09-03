cdfae10178c247c46a9d0157c93ad1d92b70eeca
DiegoMiranda5 <diegomiranda610@gmail.com>
2023-09-03 13:51:30 -0300

    Se realizo la actividad extra

---
+++ Clase3/Clase3-Act3/app.js
@@ -0,0 +1,39 @@
+let x, y;
+
+x = 200;
+y = 55.56;
+
+console.log(
+    `Suma\n` +
+    `${x} + ${y} = ${x + y}
+`);
+
+console.log(
+    `Resta\n` +
+    `${x} - ${y} = ${x - y}
+`);
+
+console.log(
+    `Multiplicacion\n` +
+    `${x} * ${y} = ${x * y}
+`);
+
+console.log(
+    `Division\n` +
+    `${x} / ${y} = ${x / y}
+`);
+
+console.log(
+    `Modulo\n` +
+    `${x} % ${y} = ${x % y}
+`);
+
+console.log(
+    `es Mayore\n` +
+    `${x} > ${y} = ${x > y}
+`);
+
+console.log(
+    `es Menor\n` +
+    `${x} < ${y} = ${x < y}
+`);
